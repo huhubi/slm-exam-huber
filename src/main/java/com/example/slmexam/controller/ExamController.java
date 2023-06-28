22a4433e19cdfef41f8bcf0e31d2a46c88d0fe51
Matthias <wi22b112@technikum-wien.at>
2023-06-28 18:14:25 +0200

    created calculatedFactorial

--- src/main/java/com/example/slmexam/controller/ExamController.java
+++ src/main/java/com/example/slmexam/controller/ExamController.java
@@ -1,22 +1,33 @@
 package com.example.slmexam.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExamController {
 
-    private String test = "hello world";
 
-    public String getStatus() {
-        return test;
-    }
-    public void setStatus(String test) {
-        this.test = test;
+    private int num = 0;
+
+    public int getNum() {
+        return num;
     }
 
-    @RequestMapping("/test")
-    public String showTest(){
-        return getStatus();
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @RequestMapping("/factorial")
+    public int calculateFactorial(@RequestParam(required = true, name = "number") int n) {
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
+
+
+
