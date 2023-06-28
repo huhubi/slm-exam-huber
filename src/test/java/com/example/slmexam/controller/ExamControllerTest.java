4164f36d716ecc0b21c5bec78ca07a4a71d571f1
Matthias <wi22b112@technikum-wien.at>
2023-06-28 18:37:26 +0200

    created Tests for factorial functions

--- src/test/java/com/example/slmexam/controller/ExamControllerTest.java
+++ src/test/java/com/example/slmexam/controller/ExamControllerTest.java
@@ -6,6 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExamControllerTest {
 
+    @Test
+    public void testCalculateFactorial() {
+        ExamController examController = new ExamController();
+        assertEquals(120, examController.calculateFactorial(5));
+    }
+
+    @Test
+    public void testGetTotalFactorial() {
+        ExamController examController = new ExamController();
+        examController.calculateFactorial(5);
+        examController.calculateFactorial(6);
+        assertEquals(840, examController.getTotalFactorial());
+    }
+
+    @Test
+    public void testAngabe() {
+        ExamController examController = new ExamController();
+        examController.calculateFactorial(4);
+        examController.calculateFactorial(5);
+        assertEquals(144, examController.getTotalFactorial());
+    }
+
+
 
 }
 
